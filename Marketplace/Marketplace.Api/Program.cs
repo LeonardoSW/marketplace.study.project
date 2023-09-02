6a3df23fa1c44080c370fe398422381c1126a2d3
Leonardo Silva Walder <42324501+LeonardoSW@users.noreply.github.com>
2023-09-02 12:26:51 -0300

    open swagger on everyenvironment

--- Marketplace/Marketplace.Api/Program.cs
+++ Marketplace/Marketplace.Api/Program.cs
@@ -26,11 +26,11 @@ builder.Services.AddValidatorsFromAssemblyContaining<NewUserInputValidator>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
